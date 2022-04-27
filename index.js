0341c3c017a23c3807f3d8f4382312908ad4d5de
Omar <otantawy53@gmail.com>
2022-04-27 11:37:43 -0400

    commit tree 4555f5f1457af5a9a3dc226a1ff877434d52d689

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let newArray = [];
+
+function writeCards(array, event){
+  for(let c = 0; c < array.length; c++){
+    let returnMessage = `Thank you, ${array[c]}, for the wonderful ${event} gift!`;
+    newArray.push(returnMessage)
+  }
+  return newArray;
+}
+
+function countDown(n){
+  while (n>=0){
+
+    console.log(n--)
+  }
+}
